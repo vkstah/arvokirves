99658926773401c3fae3d08d59f4b6fca4e5c742
Vili Ståhlberg <vili@sivulabra.fi>
2023-02-14 13:42:51 +0200

    Work on NumberField component

---
+++ src/lib/components/input/NumberField.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  export let value: number;
+  export let placeholder: string = "";
+  export let min: number | null;
+  export let max: number | null;
+  export let step: number = 1;
+  export let unit: string = "";
+</script>
+
+<div class="field">
+  <input type="number" bind:value {min} {max} {placeholder} {step} />
+  <span class="unit">{unit}</span>
+</div>
+
+<style lang="scss">
+  input {
+    border: none;
+    outline: none;
+    width: 100%;
+    height: 100%;
+  }
+
+  input,
+  .unit {
+    background-color: inherit;
+    color: var(--color-paragraph);
+    font-size: 16px;
+  }
+
+  // The height of these elements must match.
+  .field,
+  input {
+    height: 42px;
+  }
+
+  .field {
+    background-color: var(--color-input);
+    font-weight: 500;
+    display: flex;
+    align-items: center;
+    border-radius: 4px;
+  }
+
+  input {
+    padding-left: 1rem;
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+
+  .unit {
+    padding-right: 1rem;
+    cursor: default;
+    min-width: 60px;
+    text-align: right;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,64 +1,65 @@
 <script>
-	import NavCard from '$lib/components/NavCard.svelte';
-	import DiamondIcon from '$lib/icons/DiamondIcon.svelte';
-	import GraphIcon from '$lib/icons/GraphIcon.svelte';
+  import NumberField from "$lib/components/input/NumberField.svelte";
+  import NavCard from "$lib/components/NavCard.svelte";
+  import DiamondIcon from "$lib/icons/DiamondIcon.svelte";
+  import GraphIcon from "$lib/icons/GraphIcon.svelte";
 </script>
 
 <svelte:head>
-	<title>Arvokirves</title>
+  <title>Arvokirves</title>
 </svelte:head>
 
 <div class="header">
-	<h1>Arvokirves</h1>
-	<p>
-		Arvokirves on kokoelma osake- ja rahastosäästäjälle suunnattuja työkaluja. Voit käyttää
-		työkaluja säästämissuunnitelmasi tekemiseen tai vaikkapa osakepoiminnan tukena.
-	</p>
+  <h1>Arvokirves</h1>
+  <p>
+    Arvokirves on kokoelma osake- ja rahastosäästäjälle suunnattuja työkaluja. Voit käyttää
+    työkaluja säästämissuunnitelmasi tekemiseen tai vaikkapa osakepoiminnan tukena.
+  </p>
 </div>
 
 <div class="cards">
-	<NavCard
-		title="Korkoa korolle -laskuri"
-		description="Laske korkoa korolle -ilmiön avulla kerrytetyt tuotot sijoituksillesi."
-		link="/korkoa-korolle"
-		color="blue"
-	>
-		<GraphIcon size="32" />
-	</NavCard>
-	<NavCard
-		title="DCF-valuaatio"
-		description="Arvioi osakkeen käypää arvoa diskontatulla kassavirta-analyysimenetelmällä."
-		link="/dcf-valuaatio"
-		color="green"
-	>
-		<DiamondIcon size="32" />
-	</NavCard>
+  <NavCard
+    title="Korkoa korolle -laskuri"
+    description="Laske korkoa korolle -ilmiön avulla kerrytetyt tuotot sijoituksillesi."
+    link="/korkoa-korolle"
+    color="blue"
+  >
+    <GraphIcon size="32" />
+  </NavCard>
+  <NavCard
+    title="DCF-valuaatio"
+    description="Arvioi osakkeen käypää arvoa diskontatulla kassavirta-analyysimenetelmällä."
+    link="/dcf-valuaatio"
+    color="green"
+  >
+    <DiamondIcon size="32" />
+  </NavCard>
 </div>
 
 <style lang="scss">
-	.header {
-		margin-top: 3rem;
-		margin-bottom: 4rem;
+  .header {
+    margin-top: 3rem;
+    margin-bottom: 4rem;
 
-		h1 {
-			color: var(--color-white);
-		}
+    h1 {
+      color: var(--color-white);
+    }
 
-		p {
-			color: var(--color-paragraph);
-			max-width: 800px;
-			line-height: 26px;
-			margin-top: 1rem;
-		}
-	}
+    p {
+      color: var(--color-paragraph);
+      max-width: 800px;
+      line-height: 26px;
+      margin-top: 1rem;
+    }
+  }
 
-	.cards {
-		display: grid;
-		grid-template-columns: 1fr;
-		gap: 2rem;
+  .cards {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 2rem;
 
-		@media (min-width: 1200px) {
-			grid-template-columns: 1fr 1fr 1fr;
-		}
-	}
+    @media (min-width: 1200px) {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
 </style>
