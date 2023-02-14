3e095b68dabc8b34509c0d15c952e34a8e56298e
Vili Ståhlberg <vili@sivulabra.fi>
2023-02-14 13:11:54 +0200

    Update generic error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,32 +1,32 @@
 <script>
-	import { page } from '$app/stores';
+  import { page } from "$app/stores";
 </script>
 
 <div>
-	<h1>{$page.status}: {$page.error?.message}</h1>
-	<p>Hups! Jokin meni pieleen. <a href="/">Palaa etusivulle?</a> 🤔</p>
+  <h1>{$page.status}: {$page.error?.message}</h1>
+  <p>Hups! Jokin meni pieleen. <a href="/">Palaa etusivulle?</a> 🤔</p>
 </div>
 
 <style lang="scss">
-	div {
-		height: 90vh;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-	}
+  div {
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-	h1 {
-		color: var(--color-white);
-		font-size: 24px;
-	}
+  h1 {
+    color: var(--color-white);
+    font-size: 24px;
+  }
 
-	p {
-		color: var(--color-paragraph);
-		margin-top: 0.75rem;
-	}
+  p {
+    color: var(--color-paragraph);
+    margin-top: 0.75rem;
+  }
 
-	a {
-		color: var(--color-white);
-	}
+  a {
+    color: var(--color-white);
+  }
 </style>
